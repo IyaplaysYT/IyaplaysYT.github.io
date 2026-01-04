f79a817037ff71e23d731cf05c64630fbdf76473
Iyad <105991867+IyaplaysYT@users.noreply.github.com>
2026-01-04 21:20:44 +0100

    removed clutter

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function toggleDarkMode() {
-    console.log("Dark mode toggled");
     document.body.classList.toggle("dark-mode");
     document.getElementById("darkmode").classList.toggle("dark-mode-button");
     document.getElementById("dmimg").classList.toggle("dark-mode-image");
