05750b28af12e6e563caac9a010a886ccc3d147b
Iyad <105991867+IyaplaysYT@users.noreply.github.com>
2026-01-04 20:38:04 +0100

    Scrapped everything, started from scratch

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-// Function to save text to localStorage
-function saveText(event) {
-    event.preventDefault(); // Prevent form submission
-    var name = document.getElementById("name").value;
-    var location = document.getElementById("location").value;
-    var exp = document.getElementById("exp").value;
-    localStorage.setItem("name", name);
-    localStorage.setItem("location", location);
-    localStorage.setItem("exp", exp);
-    document.getElementById("output").innerText += "\nName: " + name + "\nLocation: " + location + "\n Experience:\n" + exp;
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+    document.getElementById("darkmode").classList.toggle("dark-mode-button");
+    document.getElementById("dmimg").classList.toggle("dark-mode-image");
 }
-
-// Check if there's saved text on page load
-window.onload = function() {
-    var name = localStorage.getItem("name");
-    var location = localStorage.getItem("name");
-    var exp = localStorage.getItem("name");
-    if(name) {
-        document.getElementById("output").innerText += "\nName: " + name + "\nLocation: " + location + "\n Experience:\n" + exp;
-    }
-}
-
-// Add event listener to form submission
-document.getElementById("textForum").addEventListener("submit", saveText);
