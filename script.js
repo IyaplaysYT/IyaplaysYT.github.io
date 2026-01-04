f1fccbc23e54381c0a6acbc2d06a3d523a7e2aa6
Iyad <105991867+IyaplaysYT@users.noreply.github.com>
2026-01-04 20:47:31 +0100

    fixed?

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function toggleDarkMode() {
+    console.log("Dark mode toggled");
     document.body.classList.toggle("dark-mode");
     document.getElementById("darkmode").classList.toggle("dark-mode-button");
     document.getElementById("dmimg").classList.toggle("dark-mode-image");
