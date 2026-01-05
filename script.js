703566afcae6dc28c47edaa313f81dfec825258f
Iyad <105991867+IyaplaysYT@users.noreply.github.com>
2026-01-05 21:50:11 +0100

    Added navbar and proper dark mode function

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
-    document.getElementById("darkmode").classList.toggle("dark-mode-button");
-    document.getElementById("dmimg").classList.toggle("dark-mode-image");
 }
